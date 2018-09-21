91b5f31b1bc7ecd0df04c7e4a6bf45da80d220f7
Alekxandr <alkv84@yandex.ru>
2018-09-21 14:50:05 +0300

    Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$('document').ready(function(){
+
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        lazyLoad: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        dots: true
+    });
+	
+});
